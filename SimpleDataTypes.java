6a7e46f60798deac68a236a1767842e241fc0510
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-09-15 22:53:00 -0400

    content

---
+++ SimpleDataTypes.java
@@ -0,0 +1,10 @@
+public class SimpleDataTypes {
+
+    public static void main(String[] args) {
+        int age = 35;
+        double salary = 118.5;
+        boolean busy = true;
+
+        System.out.println(age);
+    }
+}
