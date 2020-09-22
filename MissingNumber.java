3f414fd738076a42a5541b8473f3f489cdc82e42
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-09-21 23:43:02 -0400

    missing number

---
+++ MissingNumber.java
@@ -0,0 +1,10 @@
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
