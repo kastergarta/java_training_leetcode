8adc4e721452947e8c903cefc67838348017957c
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-10-12 23:19:03 -0400

    valid parent

---
+++ ValidParent.java
@@ -0,0 +1,19 @@
+public class ValidParent {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length() + 1];
+        int top = 1;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack[top++] = c; 
+            } else if (c == ')' && stack[--top] != '(') {
+                return false;
+            } else if (c == ']' && stack[--top] != '[') {
+                return false;
+            } else if (c == '}' && stack[--top] != '{') {
+                return false;
+            }
+        }
+        return top == 1;
+    }
+}
+
