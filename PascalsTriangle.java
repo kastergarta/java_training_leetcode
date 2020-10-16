c03f60a6f132a195a39cf3cbac7fad9119fb3c29
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-10-15 23:04:44 -0400

    pascals triangle

---
+++ PascalsTriangle.java
@@ -0,0 +1,19 @@
+class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows == 0) return Collections.emptyList();
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> sub = new ArrayList<>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    sub.add(1);
+                } else {
+                    List<Integer> upSub = list.get(i - 1);
+                    sub.add(upSub.get(j - 1) + upSub.get(j));
+                }
+            }
+            list.add(sub);
+        }
+        return list;
+    }
+}
