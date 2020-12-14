2b89d67604a8edcb5b7911e17a6e5d2dfd9d33db
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-12-13 19:03:01 -0500

    min stack

---
+++ MinStack.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.size() == 0 || x <= minStack.peek()) minStack.push(x);
+        else minStack.push(minStack.peek());
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
